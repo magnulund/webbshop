1c34ad1eb436fcf627b866d1ff07f8c3ecd4706b
Simon Nordlund Magnusson <Simon.Nordlund.Magnusson@gmail.com>
2026-04-21 18:07:16 +0200

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+let array = [];
+const nycklar = "items"
+
+
+function sendToCart(){
+    if(window.location.href = "index.html"){
+        window.location.assign("utcheckning.html")
+    }
+}
+function sendToMain(){
+    if(window.location.href = "utcheckning.html"){
+        window.location.assign("index.html")
+    }
+}
+
+
+function add_shit(){
+    window.localStorage.setItem
+};

f96ff7104cc514916c58850c36563356f231125a
magnulund <simon.nordlund.magnusson@gmail.com>
2026-04-27 06:57:57 +0200

    made a working system for cart

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let array = [];
 const nycklar = "items"
-const produktName = document.querySelector(".namn")
-const produktPrice = document.querySelector(".pris")
 const main = document.querySelector("main")
 function sendToCart(){
     if(window.location.href = "index.html"){
@@ -22,35 +20,65 @@ function sendBack(){
     window.location.assign("index.html")
 }
 
-function add_shit(){
 
+
+
+
+
+
+function add_shit(name, price, imgSrc){
     let produkt ={
-        namn:produktName.value,
-        pris:produktPrice.value
+        namn: name,
+        pris: price,
+        bild: imgSrc
     }
     array.push(produkt)
     let json = JSON.stringify(array)
     window.localStorage.setItem(nycklar, json)
     console.log(localStorage)
-};
-function load_from_ls(){
+}
+function displayCart(){
     let json = window.localStorage.getItem(nycklar)
 
     if (json != null){
         array = JSON.parse(json)
     }
 
-    for (let index = 0; index < array.length; index++)
-    {
+    let container;
+    if (window.location.href.includes("index.html")) {
+        container = document.querySelector("#cart");
+    } 
+    else {
+        container = main;
+    }
+
+    container.innerHTML = '<h2>Your Cart</h2>';
+
+    for (let index = 0; index < array.length; index++){
         const element = array[index]
+        let img = document.createElement("img")
+        img.src = element.bild
+        img.style.width = "100px"
         let h2 =  document.createElement("h2")
         h2.textContent = element.namn
         let h3pris = document.createElement("h3")
         h3pris.textContent = element.pris
+        let removeBtn = document.createElement("button")
+        removeBtn.textContent = "Remove"
+        removeBtn.onclick = function() { removeItem(index) }
         let div = document.createElement("div")
-        main.append(div)
+        container.append(div)
+        div.append(img)
         div.append(h2)
         div.append(h3pris)
+        div.append(removeBtn)
     }
+    
 
+}
+function removeItem(index){
+    array.splice(index, 1)
+    let json = JSON.stringify(array)
+    window.localStorage.setItem(nycklar, json)
+    displayCart()
 }
